39040bf4d2582ac73f6fe8bf12a4f188d928c76a
ison <ramonrocharezende@gmail.com>
2024-05-28 15:06:02 -0300

    fix

--- post_install.sh
+++ post_install.sh
@@ -43,26 +43,26 @@ echo "uncoment line %wheel ALL=(ALL:ALL) ALL"
 # sudo dhcpcd
 
 # graphic interface
-sudo pacman -S xorg-server xorg-xinit xorg-apps mesa
+execute_command sudo pacman -S xorg-server xorg-xinit xorg-apps mesa
 
 # drivers
 # sudo pacman -S xf86-video-intel
-sudo pacman -S nvidia nvidia-settings
+execute_command sudo pacman -S nvidia nvidia-settings
 # sudo pacman -S xf86-video-amdgpu
 
 
 # desktop environment
-pacman -S firefox #browser
-pacman -S dolphin #file manager
-pacman -S wofi #application starter
-pacman -S hyprpaper #application starter
-pacman -S alacritty #terminal emulator
+execute_command pacman -S firefox #browser
+execute_command pacman -S dolphin #file manager
+execute_command pacman -S wofi #application starter
+execute_command pacman -S hyprpaper #application starter
+execute_command pacman -S alacritty #terminal emulator
 
-pacman -S gnome-extra gnome-terminal
-pacman -S deepin deepin-extra
-pacman -S bungie-desktop
-pacman -S i3-wm i3status i3lock i3-gaps dmenu termite dunst
-pacman -S hyprland hyprland-git
+execute_command pacman -S gnome-extra gnome-terminal
+execute_command pacman -S deepin deepin-extra
+execute_command pacman -S bungie-desktop
+execute_command pacman -S i3-wm i3status i3lock i3-gaps dmenu termite dunst
+execute_command pacman -S hyprland hyprland-git
 
 
-cp config_files/scripts/* ~
+execute_command cp config_files/scripts/* ~
