0c9c3af57427a1ff1bc8134527776eb996c69944
ison <ramonrocharezende@gmail.com>
2024-05-28 15:27:30 -0300

    hyprland script

--- post_install.sh
+++ post_install.sh
@@ -60,8 +60,8 @@ execute_command sudo pacman -Sy alacritty #terminal emulator
 
 execute_command sudo pacman -Sy gnome-extra gnome-terminal
 execute_command sudo pacman -Sy budgie-desktop
-execute_command sudo pacman -Sy i3-wm i3status i3lock i3-gaps dmenu termite dunst
-execute_command sudo pacman -Sy hyprland hyprland-git
+execute_command sudo pacman -Sy i3-wm i3status i3lock i3-gaps dmenu dunst
+execute_command sudo pacman -Sy hyprland
 execute_command sudo pacman -Sy deepin deepin-extra
 
 
